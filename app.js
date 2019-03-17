b97f04388c7d1d80c4b3af7d1346653ce30989dc
Janchorizo <jancho@usal.es>
2019-03-17 12:33:28 +0100

    Poner naves en la plataforma de lanzamiento.

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 function drag(ev) {
   ev.dataTransfer.setData("text", ev.target.id);
 }
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drop(ev) {
+  ev.preventDefault();
+
+  const id = ev.dataTransfer.getData("text");
+  const newObject = document.getElementById(id).cloneNode(true);
+  newObject.id = "";
+
+  if(ev.target.hasChildNodes()){
+  	const child = ev.target.children[0];
+  	ev.target.removeChild(child);
+  }
+
+  ev.target.appendChild(newObject);
+}
